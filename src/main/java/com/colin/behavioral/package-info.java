48b675b651bc51a18901d543ccbba5102412bf9c
“zhanghy <384414328@qq.com>
2020-05-25 18:43:26 +0800

    三种设计模式类型

---
+++ src/main/java/com/colin/behavioral/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Description: 行为型模式(Behavioral Pattern)是对在不同的对象之间划分责任和算法的抽象化。
+ * <p>
+ * 行为型模式不仅仅关注类和对象的结构，而且重点关注它们之间的相互作用。
+ *
+ * @author zhy
+ * @date 2020/5/25
+ */
+package com.colin.behavioral;
---
+++ src/main/java/com/colin/creational/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Description: 创建型模式(Creational Pattern)对类的实例化过程进行了抽象，能够将软件模块中对象的创建和对象的使用分离。
+ *
+ * @author zhy
+ * @date 2020/5/25
+ */
+package com.colin.creational;
